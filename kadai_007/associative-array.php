e64111fd28b2112c55e2e6242c0e51126d30c927
yuuuki0211 <yu02110211yu@gmail.com>
2024-09-04 10:29:56 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <p>
+    <?php
+    $array = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+
+    print_r($array);
+    ?>
+  </p>
+  
+</body>
+</html>
