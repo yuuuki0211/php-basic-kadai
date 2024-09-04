5e96b11a03b8ce7760d4aa6bf1fb5f01bb3cd54b
yuuuki0211 <yu02110211yu@gmail.com>
2024-09-04 09:36:04 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <p>
+    <?php
+    $score1 = 80;
+    $score2 = 60;
+    $score3 = 55;
+    $score4 = 40;
+    $score5 = 100;
+    $score6 = 25;
+    $score7 = 80;
+    $score8 = 95;
+    $score9 = 30;
+    $score10 = 60;
+
+    echo $total_score = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+    ?>
+    </p>
+    <p>
+      <?php
+
+    echo $average = $total_score / 10;
+    ?>
+  </p>
+  
+</body>
+</html>
